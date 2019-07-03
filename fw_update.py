8a36741e3502f3c5e9c1f01caee09c8c1fae9818
SpicyCarrot1550 <SpicyCarrot1550@gmail.com>
2019-07-02 22:41:04 -0400

    Integrate flash_firmware.sh into fw_update.py

--- fw_update.py
+++ fw_update.py
@@ -1,16 +1,17 @@
 #!/bin/python
+#This script should place the Mod-T into DFU mode and flash the firmware
+#Usage: fw_update.py filename.dfu
 import sys
 import os
 import usb.core
 import usb.util
 import time
-#This script *SHOULD* eventually be the all-encompassing firmware update
-#It should call the check FW script, place the Mod-T into DFU mode and flash the firmware
-#It should have a command-line arg to flash older firmware versions, none of this is really implemented yet
-#Make sure this was called correctly
+import subprocess
+
+#Make sure this script was called correctly
 if not len(sys.argv)==2:
-	print("Usage: fw_update.py filename.dfu")
-	quit()
+    print("Usage: fw_update.py filename.dfu")
+    quit()
 
 #Find the Mod-T - we should probably see if it's in DFU mode, too
 #That way we can do emergency flashes from recovery mode
@@ -18,13 +19,13 @@ dev = usb.core.find(idVendor=0x2b75, idProduct=0x0002)
 
 #If we didn't find a Mod-T we need to throw an error
 if dev is None:
-	raise ValueError('No Mod-T detected')
+    raise ValueError('No Mod-T detected')
 
 #Make sure the filename supplied is actually a file, and error out appropriately
 fname=str(sys.argv[1])
 if not os.path.isfile(fname):
-	print(fname + " not found")
-	quit()
+    print(fname + " not found")
+    quit()
 
 #Set active configuration (first is default)
 dev.set_configuration()
@@ -38,3 +39,32 @@ dev.write(2, '{"transport":{"attrs":["request","twoway"],"id":7},"data":{"comman
 #Wait for the Mod-T to reattach in DFU mode
 time.sleep(2)
 
+#Open log file
+log = open("/tmp/dfu", "w+")
+
+#Start writing the firmware, and write the output to the log file.
+subprocess.popen(["dfu-util", "-d", "2b75:0003", "-a", "0", "-s", "0x0:leave", "-D", fname], stdout=log, stderr=subprocess.STDOUT)
+
+#Loop until the firmware has been written
+while True:
+    #Steal just the progress value from the file
+    tac = subprocess.popen(["tac", "/tmp/dfu"], stdout=PIPE)
+    egrep = subprocess.popen(["egrep", "-m", "1", "."], stdin=tac.stdout, stdout=pipe)
+    sed = subprocess.popen(["sed", "s/.*[ \t][ \t]*\([0-9][0-9]*\)%.*/\1/"], stdin=egrep.stdout, stdout=PIPE)
+    progress = sed.communicate()[0]
+
+    #If dfu-util reports transitioning, we've finished
+    if "Transitioning" in progress:
+        #We won't always capture the 100% progress indication, so we force it
+        progress = 100
+        print(progress)
+        #exit the loop
+        break
+    #Write the progress as we get updates
+    print(progress)
+    #the dfu-util write is kinda slow, let's not waste too much cpu time
+    time.sleep(1)
+
+#cleanup our temporary file
+log.close()
+os.remove /tmp/dfu
