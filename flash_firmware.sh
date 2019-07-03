4c20d8fdddfe6d43a60da77323598997296d3bf2
SpicyCarrot1550 <SpicyCarrot1550@gmail.com>
2019-07-02 22:35:55 -0400

    Change hardcoded path to an argument and update copyright

--- flash_firmware.sh
+++ flash_firmware.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 #This script actually runs dfu-util to flash firmware to the Mod-T
-#Currently the firmware version and location is hard-coded for testing purposes. Eventually this should be changed to $1
+
+#Test if the number of arguments is not equal to 1, or the first argument is not a regular file
+if [ $# -ne 1 ] || [ ! -f "$1" ]; then
+    echo "Usage: flash_firmware.sh PATH_TO_DFU_FILE"
+    exit 1
+fi
+
 #Actually start writing the firmware, in the background, and log to a file.
-dfu-util -d 2b75:0003 -a 0 -s 0x0:leave -D /home/xaero/Downloads/firmware_modt_override.dfu > /tmp/dfu &
+dfu-util -d 2b75:0003 -a 0 -s 0x0:leave -D $1 > /tmp/dfu &
 
 #Loop until the firmware has been written
 while true; do
