dff0c10f7d1fe365db8f6aca2c2419cd88e8ebb1
SpicyCarrot1550 <SpicyCarrot1550@gmail.com>
2019-10-18 23:17:46 -0400

    Fixed the status script

--- Better Interface/modt_status.py
+++ Better Interface/modt_status.py
@@ -1,32 +1,34 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-# Requires pyusb and permissions to read/write the mod-t via USB.
-# Just polls the Mod-T for status JSON
+import usb.core #for communicating with the MOD-t
+import json #for parsing JSON read from the MOD-t
+import pprint #for pretty printing status
+import time #for sleeping
 
-import sys
-import os
-import usb.core
-import usb.util
-import time
 
-# Read pending data from MOD-t (bulk reads of 64 bytes)
-def read_modt(ep):
- text=''.join(map(chr, dev.read(ep, 64)))
- fulltext = text
- while len(text)==64:
-        text=''.join(map(chr, dev.read(ep, 64)))
-        fulltext = fulltext + text
- return fulltext
-
-# Find MOD-t usb device
+# Find a MOD-t usb device
 dev = usb.core.find(idVendor=0x2b75, idProduct=0x0002)
-
-# was it found?
+# Throw an exception if a MOD-t was not found
 if dev is None:
-    raise ValueError('Device not found')
+    raise ValueError('MOD-t not found')
 
-#Finally, loop and query mod-t status every 5 seconds 
-while True:
- dev.write(4, '{"metadata":{"version":1,"type":"status"}}')
- print(read_modt(0x83))
- time.sleep(5)
+# Read pending data from the MOD-t (bulk reads of 64 bytes from endpoint 0x83)
+def read_status():
+    # Send this to the MOD-t on endpoint 4; this apparently makes it queue the status data
+    dev.write(4, '{"metadata":{"version":1,"type":"status"}}')
+    # Read 64 bytes of data from endpoint 0x83
+    text=''.join(map(chr, dev.read(0x83, 64)))
+    fulltext = text
+    # Loop until we have the entire status
+    while len(text)==64:
+        text=''.join(map(chr, dev.read(0x83, 64)))
+        fulltext = fulltext + text
+    # Parse the JSON and store it as a python dictionary
+    return(json.loads(fulltext))
+
+
+# If __main__, then pretty print status every five seconds
+while __name__ == '__main__':
+    pprint.pprint(read_status())
+    time.sleep(5)
+    print('\n\n\n\n') # For readability
